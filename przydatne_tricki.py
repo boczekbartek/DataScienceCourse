58701512b206b82ccbd57ec5e3c67081b41a016d
boczekbartek <goterix1@gmail.com>
2016-11-24 00:40:47 +0100

    zawiera ciekawe tricki z pythona

---
+++ przydatne_tricki.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+###########################################
+number = 0
+print number, #drukuje number BEZ znaku nowej linii na końcu
+
+print number #drukuje number ze znakiem nowej linii na końcu
+
+print "o patrzcie, w nowej linii się druknęło"
+
+import numpy as np
+
+A = np.random.normal()
